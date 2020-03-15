6139391fa872c0f18d5cffab0e1cc703b4402ded
romakvadrat <romakvadrat@gmail.com>
2020-03-15 14:59:58 +0000

    calc

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 const display = document.querySelector('.display');
 
 document.querySelectorAll('.digits button')
-.forEach( button => button.addEventListener('click', digitPressed));
+    .forEach( button => button.addEventListener('click', digitPressed));
 
 function digitPressed(ev) {
     ev.preventDefault();
     display.value += ev.target.innerText;
 }
+
+document.querySelectorAll('.opers button')
+    .forEach( button => button.addEventListener('click', opersPressed));
+
+function opersPressed(ev) {
+    ev.preventDefault();
+    display.value += ev.target.innerText;
+}
+
+document.querySelector('.back').addEventListener('click', back);
+function back() {
+    var exp = display.value;
+    display.value = exp.substring(0, exp.length - 1);
+}
+
+document.querySelector('.eq').addEventListener('click', calc);
+     function calc() {
+         display.value = eval(display.value);   
+         
+}
+
+
